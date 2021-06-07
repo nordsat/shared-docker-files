411df41973c07b99b4f1013da44e423f34e79532
Panu Lahtinen <pnuu+git@iki.fi>
2021-06-07 15:23:03 +0300

    Do not create a new ssh key at startup

--- trollmoves/build_dir/entrypoint.sh
+++ trollmoves/build_dir/entrypoint.sh
@@ -10,6 +10,6 @@ echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
 chown -R ${USER_ID}.${USER_ID} /home/user/
-su user -c 'ssh-keygen -f /home/user/.ssh/id_rsa -N ""'
-cp /home/user/.ssh/id_rsa.pub /mnt/config/
+mkdir -p /home/user/.ssh
+echo "StrictHostKeyChecking no" >> /home/user/.ssh/config
 exec gosu user "$@"
