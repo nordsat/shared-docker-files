b71af8401fd188ca9cdf60c3ee0a94409bd65724
Panu Lahtinen <pnuu+git@iki.fi>
2021-06-10 15:22:56 +0300

    Use common base images and add other layers on top of those

--- trollflow2_full_image/build_dir/entrypoint.sh
+++ trollflow2_full_image/build_dir/entrypoint.sh
@@ -10,4 +10,8 @@ echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
 chown -R ${USER_ID}.${USER_ID} /home/user/
-exec gosu user "$@"
+mkdir -p /home/user/.ssh
+echo "StrictHostKeyChecking no" >> /home/user/.ssh/config
+echo ". /opt/conda/etc/profile.d/conda.sh" >> /home/user/.bashrc && \
+echo "conda activate base" >> /home/user/.bashrc
+exec gosu user bash -c 'source /home/user/.bashrc && supervisord -c /mnt/config/supervisord.conf'
