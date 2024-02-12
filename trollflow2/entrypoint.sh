a9f7dde5c56768729273c9f4f085bfa5e62d920d
Martin Raspaud <martin.raspaud@smhi.se>
2024-02-12 17:39:04 +0100

    Remove log config for trollflow2 entrypoint

--- trollflow2/entrypoint.sh
+++ trollflow2/entrypoint.sh
@@ -4,4 +4,4 @@ source /opt/conda/.bashrc
 source /config/env-variables
 
 micromamba activate
-/opt/conda/bin/satpy_launcher.py -n false -a ${MESSAGE_SOURCE} -c /config/trollflow2_log_config.yaml /config/trollflow2.yaml
+/opt/conda/bin/satpy_launcher.py -n false -a ${MESSAGE_SOURCE} /config/trollflow2.yaml
